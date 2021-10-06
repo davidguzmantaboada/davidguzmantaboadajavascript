427a1bbb1d72e1a13837a5e6401dc7f0278a4ea1
davidguzmantaboada <90436734+davidguzmantaboada@users.noreply.github.com>
2021-10-05 20:39:48 -0500

    initial commit

---
+++ codigo.js
@@ -0,0 +1,5 @@
+let ingreseNumero=parseInt(prompt("Ingrese un Numero"));
+for (let i = 1; i <= 10; i++){
+	let resultado = ingreseNumero + i ;
+	alert (ingreseNumero + " + " + i + " = " + resultado);
+}
