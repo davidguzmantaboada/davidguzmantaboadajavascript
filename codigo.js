da2016f6d8866aa816f57b4e062638e97824efdf
davidguzmantaboada <90436734+davidguzmantaboada@users.noreply.github.com>
2021-10-19 22:35:57 -0500

    Delete codigo.js

--- codigo.js
+++
@@ -1,5 +0,0 @@
-let ingreseNumero=parseInt(prompt("Ingrese un Numero"));
-for (let i = 1; i <= 10; i++){
-	let resultado = ingreseNumero + i ;
-	alert (ingreseNumero + " + " + i + " = " + resultado);
-}
